4f8d2ace08130be648ef815c2797df0ecb0c0b05
web-mrAlderson <tima-999@inbox.ru>
2020-10-15 18:38:11 +0300

    fix: build

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+import Imput from './Input';
+import './style.css';
+
+
+function App() {
+    return (
+       <div className='wrapper'>
+           <Imput/>
+       </div>
+   )
+};
+
+export default App
