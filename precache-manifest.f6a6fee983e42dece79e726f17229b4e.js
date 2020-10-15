d74b03c597cf624bc5fcfef427e4d398e00243d5
web-mrAlderson <tima-999@inbox.ru>
2020-10-15 19:00:59 +0300

    fix: build

---
+++ precache-manifest.f6a6fee983e42dece79e726f17229b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74dcae25e2691292e96428a83740f8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a9dc3d8c169c3b11a88",
+    "url": "/static/css/main.f39dfd88.chunk.css"
+  },
+  {
+    "revision": "a2275ec991be86ef5c27",
+    "url": "/static/js/2.0b51ea9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b51ea9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9dc3d8c169c3b11a88",
+    "url": "/static/js/main.8d620228.chunk.js"
+  },
+  {
+    "revision": "8a2fad8b9d2c819c8573",
+    "url": "/static/js/runtime-main.b3f3002b.js"
+  }
+]);
--- src/constants/index.js
+++
@@ -1,3 +0,0 @@
-export const BASE_PATH = 'http://speller.yandex.net/services';
-export const SEARCH_PATH = '/spellservice.json';
-export const SEARCH_PARSM = '/checkText?text=';
